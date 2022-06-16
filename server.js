ab0dd8d8e13df420dee806331a77c1f9b8d47405
Mt-Lampert <perrin.blog@web.de>
2022-06-16 16:34:15 +0200

    w: initialize backend server

---
+++ server.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+// import {express} from "express"
+const express = require("express");
+
+const { appendFile } = require("fs");
+const { clearScreenDown } = require("readline");
+const { CLIENT_RENEG_LIMIT } = require("tls");
+
+// eine Express-Instanz aufbauen
+const app = express();
+
+// Server einstellen und konfigurieren
+
+
+// Log-Middleware 
+app.use((req, res, next) => {
+  console.log(`Route: '${req.path}'; Method: ${req.method}`);
+  next();
+})
+
+app.get('/', (req, res) => {
+  res.json({
+    "message": "Es laeuft!"
+  })
+})
+
+
+// Server starten!
+app.listen(process.env.PORT, () => {
+  console.log(`Listening to port ${process.env.PORT}. Hello Folks!`)
+}) 
